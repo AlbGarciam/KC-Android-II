94a5b4a155b002a8896b2b794e1510e981f1eca6
Alberto Garcia-Muñoz <alberto.garciamunoz@vodafone.com>
2019-06-26 14:48:45 +0200

    Update entities

--- app/src/main/java/com/soundapp/mobile/todotask/data/repository/local/subtask/SubtaskEntity.kt
+++ app/src/main/java/com/soundapp/mobile/todotask/data/repository/local/subtask/SubtaskEntity.kt
@@ -1,21 +1,16 @@
 package com.soundapp.mobile.todotask.data.repository.local.subtask
 
-import androidx.room.Entity
-import androidx.room.ForeignKey
+import androidx.room.*
 import androidx.room.ForeignKey.CASCADE
-import androidx.room.PrimaryKey
 import com.soundapp.mobile.todotask.data.repository.local.task.TaskEntity
 
-@Entity(tableName = "subtasks", foreignKeys = [
-    ForeignKey(
-        entity = TaskEntity::class,
-        parentColumns = ["id"],
-        childColumns = ["parentId"],
-        onDelete = CASCADE
-    )])
+@Entity(tableName = "subtasks",
+    indices = arrayOf(Index(value = ["id", "parentId"]))
+)
 data class SubtaskEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
+    @ForeignKey(entity = TaskEntity::class, parentColumns = ["id"], childColumns = ["parentId"], onDelete = CASCADE)
     val parentId: Long,
     val content: String,
     val isFinished: Boolean
--- app/src/main/java/com/soundapp/mobile/todotask/data/repository/local/task/TaskEntity.kt
+++ app/src/main/java/com/soundapp/mobile/todotask/data/repository/local/task/TaskEntity.kt
@@ -1,10 +1,11 @@
 package com.soundapp.mobile.todotask.data.repository.local.task
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import org.threeten.bp.Instant
 
-@Entity(tableName = "tasks")
+@Entity(tableName = "tasks", indices = arrayOf(Index(value = ["id"], unique = true)))
 data class TaskEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
